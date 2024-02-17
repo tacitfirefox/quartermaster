f1cc404bd1ca63bb421d1ce34464b5794882eef4
tacitfirefox <tacitfirefox@outlook.com>
2024-02-16 19:06:58 -0500

    initial concept implementation, incomplete

---
+++ source/Quartermaster.Implementation.Rin/RinQuartermaster.cs
@@ -0,0 +1,18 @@
+﻿namespace Quartermaster.Implementation.Rin;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Quartermaster.Core;
+using Quartermaster.Core.Schema;
+
+public class RinQuartermaster : IQuartermaster
+{
+    private readonly IQuartermasterRegistration _quartermasterRegistration = new RinQuartermasterRegistration();
+
+    public Task<IQuartermasterReceipt> CheckoutRostersAsync(IEnumerable<IRoster> rostersToCheckout)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IQuartermasterRegistration> GetQuartermasterRegistrationAsync() => Task.FromResult(this._quartermasterRegistration);
+}
---
+++ source/Quartermaster.Implementation.Rin/RinQuartermasterRegistration.cs
@@ -0,0 +1,12 @@
+﻿namespace Quartermaster.Implementation.Rin;
+
+using Quartermaster.Core;
+
+public class RinQuartermasterRegistration : IQuartermasterRegistration
+{
+    private readonly Guid _quartermasterId;
+
+    public string Name => $"Rin's Travel Agency-Id {_quartermasterId}";
+
+    public string Key => _quartermasterId.ToString();
+}
